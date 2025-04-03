e9caea21bfb764790fb3302c808c13057efe9130
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-04-02 17:19:27 -0700

    updated home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Layout from './Layout.jsx';
 
 
 function Home() {
-    const [homeData, setHomeData] = useState({hero: {} });
+    const [homeData, setHomeData] = useState({hero: {}, about: {} });
 
     useEffect(() => {
         fetch('/data/homeData.json') 
@@ -17,28 +17,36 @@ function Home() {
         <Layout> 
             <div className="main-content">
                 <h1>{homeData.hero.title || "loading..."}</h1>
-                <h2>{homeData.hero.subtitle || ""}</h2>
+                  <h2>{homeData.hero.subtitle || ""}</h2>
                 <button>
                 <a href="#about">{homeData.hero.buttonText || "Click me"}</a>
                 </button>
-                <p>{homeData.hero.location}</p>
-                <img src="" alt="Mahdi's photo" />
+                 <p>{homeData.hero.location}</p>
+                < img src="/images/home/hero.jpg" alt="Mahdi's photo" />
+
+
 
                 <div className='about'>
+                <h2>{homeData.about.title || "loading..."}</h2>
+                <button>
+                    <a href="#works">View works</a>
+                </button>
+                <h3>Skills</h3>
+                <p>{homeData.about.skills}</p>
 
+                <h3>Hobbies</h3>
+                <p>{homeData.about.hobbies}</p>
                 </div>
 
                 <div className='work-section'>
-
                 </div>
 
                 <div className='education-section'>
-
                 </div>
 
                 <div className='recomendation-section'>
-
                 </div>
+
             </div>
         </Layout>
     );
