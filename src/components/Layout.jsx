fe8d02185e9a11173e26bd89f078d2ee669427a2
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-04-05 15:10:35 -0700

    update Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+import { motion } from 'framer-motion';
+import { useFollowPointer } from './useFollowPointer';
+
 function Layout({ children }) {
+    const ref = useRef(null);
+    const { x, y } = useFollowPointer(ref);
+    const location = useLocation(); 
+
+
+
     return (
         <>
             <Header />
+            <motion.div style={{ x, y }} ref={ref} className="pointer"></motion.div>
             <main>{children}</main> 
             <Footer />
         </>
--- src/components/useFollowPointer.jsx
+++ src/components/useFollowPointer.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React, { useEffect } from 'react';
+import { useMotionValue, useSpring, frame } from 'framer-motion';
 
-function Layout({ children }) {
-    return (
-        <>
-            <Header />
-            <main>{children}</main> 
-            <Footer />
-        </>
-    );
+const spring = { damping: 2, stiffness: 60, restDelta: 0.001 };
+
+export function useFollowPointer(ref) {
+    const xPoint = useMotionValue(0);
+    const yPoint = useMotionValue(0);
+    const x = useSpring(xPoint, spring);
+    const y = useSpring(yPoint, spring);
+
+    useEffect(() => {
+        const isTouchDevice = Window.matchMedia("(pointer: coarse)").matches;
+
+        if  (isTouchDevice) return;
+
+        const handelPointerMove = ({ clientX, clientY }) => {
+            if (!ref.current) returen;
+
+            const element = ref.current;
+
+            frame.read(() => {
+                xPoint.set(clientX - element.offsetLeft - element.offsetWidth / 2);
+                yPoint.set(clientY - element.offsetTop - element.offsetWidth / 2);
+            });
+        };
+
+        window.addEventListener("pointermove", handelPointerMove);
+
+        return () => window.removeEventListener("pointermove", handelPointerMove);
+    }, [xPoint, yPoint, ref]);
+
+    return { x, y };
 }
-
-export default Layout;
+  
