920c471c5891c869d6c9ba9ffcd8d4052466087c
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-04-17 16:09:19 -0700

    added email address on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,33 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../Layout/Layout.jsx';
 
-
-
-    function Contact() {
-        const [contactData, setContactData] = useState({ hero: {},});
+function Contact() {
+    const [contactData, setContactData] = useState({ hero: {},});
     
-        useEffect(() => {
-            fetch('/data/contactData.json')
-                .then(response => response.json())
-                .then(data => setContactData(data))
-                .catch(error => console.error("Error fetching data:", error));
-        }, []);
+    useEffect(() => {
+        fetch('/data/contactData.json')
+            .then(response => response.json())
+            .then(data => setContactData(data))
+            .catch(error => console.error("Error fetching data:", error));
+    }, []);
 
+    const handleEmailClick = () => {
+        const email = contactData.hero.email;
+        if (email) {
+            window.location.href = `mailto:${email}`;
+        }
+    };
 
-        return (
-            <Layout>
-                <div className="main-content">
-                    {/* Hero Section */}
-                    <h1>{contactData.hero.title || "loading..."}</h1>
-                    <h2>{contactData.hero.subtitle || "loading..."}</h2>
-                    <p>{contactData.hero.location || "loading..."}</p>
+    return (
+        <Layout>
+            <div className="main-content">
+                {/* Hero Section */}
+                <div className="hero-section">
+                    <div className="hero-content">
+                        <h1>{contactData.hero.title || "loading..."}</h1>
+                        <h2>{contactData.hero.subtitle || "loading..."}</h2>
+                        <p>{contactData.hero.location || "loading..."}</p>
+                        <button className="contact-button" onClick={handleEmailClick}>Send me a message</button>
+                    </div>
                     <img src={contactData.hero.image} alt="Hero" />
-                    
                 </div>
-            </Layout>
-        );
+            </div>
+        </Layout>
+    );
+}
 
-
-
-} 
 export default Contact;
