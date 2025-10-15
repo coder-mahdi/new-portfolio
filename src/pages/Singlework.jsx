1d4e02d4a19a6360d4408a2323fb59de78f0e2b8
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-10-14 22:48:48 -0700

    added new work data

--- src/pages/Singlework.jsx
+++ src/pages/Singlework.jsx
@@ -7,6 +7,11 @@ function SingleWork() {
     const { id } = useParams();
     const [project, setProject] = useState(null);
 
+    // Scroll to top when component mounts or id changes
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [id]);
+
     useEffect(() => {
         const baseUrl = window.location.origin;
         fetch(`${baseUrl}/data/worksData.json`)
